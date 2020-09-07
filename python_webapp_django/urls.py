241a483f1e14a94256e8c3ea26b11ac8646b2b8f
shinjiniray <30630268+shinjiniray@users.noreply.github.com>
2020-09-07 11:38:53 +0530

    Update urls.py

--- python_webapp_django/urls.py
+++ python_webapp_django/urls.py
@@ -58,4 +58,4 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
